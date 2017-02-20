53597e48e1681199b17be148c4744e7bb74f35e8
Mike Wohlrab <Mike@Layer7.Solutions>
2017-02-19 23:46:14 -0600

    Database code updated for mod mail logging. Code for mail.replies will likely need changing as it currently doesn’t get reply data.

--- ModmailArchiver/modmail.py
+++ ModmailArchiver/modmail.py
@@ -21,7 +21,7 @@ class ModmailArchiver(object):
         except TypeError:
             return None
 
-    def gather_items(self, limit=200):
+    def gather_items(self, limit):
         arg_dicts = []
         last_seen = self.db.get_last_seen()
         if self.modMailMulti:
@@ -30,35 +30,34 @@ class ModmailArchiver(object):
             return
         try:
             for mail in mail_generator:
-                if limit and self.db.is_logged(mail.id):
-                    continue
-                #['author', 'block', 'body', 'body_html', 'context', 'created', 'created_utc', 'dest', 'distinguished', 'first_message', 'first_message_name', 'fullname', 'id', 'mark_read', 'mark_unread', 'mute', 'name', 'new', 'parent_id', 'parse', 'replies', 'reply', 'subject', 'subreddit', 'subreddit_name_prefixed', 'unmute', 'was_comment']
-                arg_dict = {
-                    "thing_id": mail.fullname,
-                    "message_root_thing_id": mail.first_message_name,
-                    "message_from": str(mail.author),
-                    "message_to": mail.dest.replace("#", "r/") if mail.dest.startswith("#") else mail.dest,
-                    "created_utc": datetime.utcfromtimestamp(mail.created_utc),
-                    "subject": mail.subject,
-                    "body": mail.body,
-                    "parent_thing_id": mail.parent_id,
-                    "subreddit": mail.subreddit
-                }
-                arg_dicts.append(arg_dict)
+                if limit and not self.db.is_logged(mail.fullname):
+                    arg_dict = {
+                        "thing_id": mail.fullname,
+                        "message_root_thing_id": mail.first_message_name,
+                        "message_from": str(mail.author),
+                        "message_to": mail.dest.replace("#", "r/") if mail.dest.startswith("#") else mail.dest,
+                        "created_utc": datetime.utcfromtimestamp(mail.created_utc),
+                        "subject": mail.subject,
+                        "body": mail.body,
+                        "parent_thing_id": mail.parent_id,
+                        "subreddit": mail.subreddit
+                    }
+                    arg_dicts.append(arg_dict)
 
                 for reply in mail.replies:
-                    reply_dict = {
-                        "thing_id": reply.fullname,
-                        "message_root_thing_id": reply.first_message_name,
-                        "message_from": str(reply.author),
-                        "message_to": reply.dest.replace("#", "r/") if reply.dest.startswith("#") else reply.dest,
-                        "created_utc": datetime.utcfromtimestamp(reply.created_utc),
-                        "subject": reply.subject,
-                        "body": reply.body,
-                        "parent_thing_id": reply.parent_id,
-                        "subreddit": reply.subreddit
-                    }
-                    arg_dicts.append(reply_dict)
+                    if limit and not self.db.is_logged(mail.fullname):
+                        reply_dict = {
+                            "thing_id": reply.fullname,
+                            "message_root_thing_id": reply.first_message_name,
+                            "message_from": str(reply.author),
+                            "message_to": reply.dest.replace("#", "r/") if reply.dest.startswith("#") else reply.dest,
+                            "created_utc": datetime.utcfromtimestamp(reply.created_utc),
+                            "subject": reply.subject,
+                            "body": reply.body,
+                            "parent_thing_id": reply.parent_id,
+                            "subreddit": reply.subreddit
+                        }
+                        arg_dicts.append(reply_dict)
 
                 mail = mail_generator.next()
 
@@ -70,6 +69,6 @@ class ModmailArchiver(object):
     def log(self, limit=100):
         arg_dicts = self.gather_items(limit)
         self.db.log_items(arg_dicts)
-        if not limit:
-            self.r.redditor('thirdegree').message('force modlog history', 'Finished for {}'.format(self.subs))
+        #if not limit:
+        #    self.r.redditor('thirdegree').message('force modmail history', 'Finished for {}'.format(self.subs))
 
