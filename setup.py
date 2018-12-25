3ddc4cefb26abb72073d7b39a49434e52dd466cc
Joel Christiansen <greenbadge.jc@gmail.com>
2018-12-25 17:53:48 -0600

    renaming package to make pylint happy

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='TheSentinel',  # Required
+    name='the_sentinel',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
