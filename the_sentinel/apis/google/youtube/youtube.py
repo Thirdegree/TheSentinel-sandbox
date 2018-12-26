4ac616232e1bab03385c01a34ebbe28d1a57923c
Joel Christiansen <greenbadge.jc@gmail.com>
2018-12-25 20:55:55 -0600

    inital youtube api work

---
+++ the_sentinel/apis/google/youtube/youtube.py
@@ -0,0 +1,72 @@
+from typing import Dict, Any
+import requests
+
+
+class Youtube(requests.Session):
+    API_BASE='https://www.googleapis.com'
+    REST_BASE=['youtube', 'v3']
+    ENDPOINT_BASE=''
+    AUTH = {}
+
+    def __init__(self, key=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if key:
+            self.AUTH['key'] = key
+        self._json: Optional[Dict[Any, Any]] = None
+        self._resp: Optional[requests.Response] = None
+
+    @property
+    def resp(self) -> requests.Response:
+        if self._resp is None:
+            self._resp = self.get()
+            self._resp.raise_for_status()
+        return self._resp
+
+    @property
+    def json(self) -> Dict[Any, Any]:
+        return self.resp.json()
+
+    def request(self, method, url='', params=None, **kwargs):
+        endpoint = '/'.join(self.REST_BASE)
+        if self.ENDPOINT_BASE:
+            endpoint += '/' + self.ENDPOINT_BASE
+        # don't care about the original url at all,
+        # don't even want to supply it
+        url = '/'.join([self.API_BASE, endpoint])
+        if params is None:
+            params = {}
+        params.update({
+            'part': 'snippet'
+            })
+        return super().request(method, url, params=params, **kwargs)
+
+    # getting rid of the requirement to supply the url
+    def get(self, url='', **kwargs):
+        return super().get(url, **kwargs)
+    def put(self, url='', **kwargs):
+        return super().put(url, **kwargs)
+    def post(self, url='', **kwargs):
+        return super().post(url, **kwargs)
+    def delete(self, url='', **kwargs):
+        return super().delete(url, **kwargs)
+
+class Channel(Youtube):
+    ENDPOINT_BASE='channels'
+    def __init__(self, channel_id, key=None, *args, **kwargs):
+        self.id = channel_id
+        super().__init__(key=key, *args, **kwargs)
+
+    def get(self, params=None, **kwargs):
+        default_params = {
+            'part': 'snippet',
+            'id': self.id,
+            # 'fields': 'items(id,snippet/title)',
+            'key': self.AUTH['key']
+            }
+
+        if params is None:
+            params = {}
+        default_params.update(params)
+        return super().get(params=default_params, **kwargs)
+
+
